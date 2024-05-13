1d9b4fb26a2d855f775204d2628706c51f4b6e42
gkrls <gkrls.codes@gmail.com>
2024-05-13 15:39:01 +0200

    small cleanup

--- src/controller/bfrt_cli_config.py
+++ src/controller/bfrt_cli_config.py
@@ -22,41 +22,26 @@ with open(CONFIG_JSON, 'r') as f:
     cfg = data[SWITCH_NAME]
 
 # get a handle to the relevant tables
-p4 = bfrt.switch.pipe
-icmp = p4.Ingress.icmp_table
-arp = p4.Ingress.arp_table
-fw = p4.Ingress.forwarding_table
+P4 = bfrt.switch.pipe
+ICMP = P4.Ingress.icmp_table
+ARP  = P4.Ingress.arp_table
+FRWD = P4.Ingress.forwarding_table
 
-# perform static mapping of switch ports to hosts (or other switches)
-# normally we would have the switch "learn" those, but for our purposes
-# a static mapping is enough.
-print(Color.BOLD + "Configuring switch forwarding:" + Color.END)
-for p in cfg["ports"]:
-    port = int(p)
-    for i, h in enumerate(cfg["ports"][p]):
-        err = None
-        mac = EUI(h['mac'])
-        try:
-            fw.add_with_forward(dst_addr=mac, port=port)
-        except Exception as e:
-            err = e
-
-        print("  %s %s (%s)" % (("port %d ->" % port) if i == 0 else " " * len("port %s ->" % p), mac, "ok" if err is None else err))
-
-
+# Configure the switch as an endpoint that can be pinged (ICMP)
+# Additionally, if enabled it can be an ARP resolver as well
 if "endpoint" in cfg and "mac" in cfg["endpoint"] and "ip" in cfg["endpoint"]:
     print(Color.BOLD + "Configuring switch endpoint:" + Color.END)
 
     err = None
-    mac = EUI(cfg["endpoint"]['mac'])
-    ip4 = IPAddress(cfg["endpoint"]['ip'])
+    sw_mac = EUI(cfg["endpoint"]['mac'])
+    sw_ip4 = IPAddress(cfg["endpoint"]['ip'])
 
     # Add an entry for the switch in the ICMP table
     try:
-        icmp.add_with_icmp_echo_response(dst_addr=ip4)
+        ICMP.add_with_icmp_echo_response(dst_addr=sw_ip4)
     except Exception as e:
         err = e
-    print("  %s / %s (%s)" % (mac, ip4, "ok" if err is None else err))
+    print("  %s / %s (%s)" % (sw_mac, sw_ip4, "ok" if err is None else err))
 
 
     if "arp-resolver" in cfg["endpoint"] and cfg["endpoint"]["arp-resolver"]:
@@ -65,10 +50,10 @@ if "endpoint" in cfg and "mac" in cfg["endpoint"] and "ip" in cfg["endpoint"]:
         # Add an ARP entry for the switch
         err = None
         try:
-            arp.add_with_arp_resolve(dst_proto_addr=ip4, mac=mac)
+            ARP.add_with_arp_resolve(dst_proto_addr=sw_ip4, mac=sw_mac)
         except Exception as e:
             err = e
-        print("  %s -> %s (%s)" % (mac, ip4, "ok" if err is None else err))
+        print("  %s -> %s (%s)" % (sw_mac, sw_ip4, "ok" if err is None else err))
 
         # Add ARP entries for each host
         for p in cfg["ports"]:
@@ -78,8 +63,25 @@ if "endpoint" in cfg and "mac" in cfg["endpoint"] and "ip" in cfg["endpoint"]:
                 ip4 = IPAddress(h['ip'])
                 mac = EUI(h['mac'])
                 try:
-                    arp.add_with_arp_resolve(dst_proto_addr=ip4, mac=mac)
+                    ARP.add_with_arp_resolve(dst_proto_addr=ip4, mac=mac)
                 except Exception as e:
                     err = e
                 print("  %s -> %s (%s)" % (mac, ip4, "ok" if err is None else err))
+
+# perform static mapping of switch ports to hosts (or other switches)
+# normally we would have the switch "learn" those, but for our purposes
+# a static mapping is enough.
+if "ports" in cfg:
+    print(Color.BOLD + "Configuring switch forwarding:" + Color.END)
+    for p in cfg["ports"]:
+        port = int(p)
+        for i, h in enumerate(cfg["ports"][p]):
+            err = None
+            mac = EUI(h['mac'])
+            try:
+                FRWD.add_with_forward(dst_addr=mac, port=port)
+            except Exception as e:
+                err = e
+
+            print("  %s %s (%s)" % (("port %d ->" % port) if i == 0 else " " * len("port %s ->" % p), mac, "ok" if err is None else err))
 
